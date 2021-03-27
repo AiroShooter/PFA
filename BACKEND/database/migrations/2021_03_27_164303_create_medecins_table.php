aeabcb40309360c8409ec5c97d0203b4583779be
ayoub <youbchaf@gmail.com>
2021-03-27 17:50:03 +0100

    migration step 1

---
+++ BACKEND/database/migrations/2021_03_27_164303_create_medecins_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMedecinsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medecins', function (Blueprint $table) {
+            $table->bigIncrements('med_id');
+            $table->unsignedBigInteger("user_id");
+            $table->foreign("user_id")->references("user_id")
+                ->on("users")->onDelete("cascade")->onUpdate("cascade");
+            $table->unsignedBigInteger("spec_id");
+            $table->foreign("spec_id")->references("spec_id")
+                ->on("specialites")->onDelete("cascade")->onUpdate("cascade");
+            $table->string('titre');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->float('tarif');
+            $table->string('siteWeb');
+            $table->string('adresseCabinet');
+            $table->string('ville');
+            $table->string('teleCabinet');
+            $table->string('telePerso');
+            $table->integer('duree');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medecins');
+    }
+}
---
+++ BACKEND/database/migrations/2021_03_27_164430_create_logs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->bigIncrements('log_id');
+            $table->unsignedBigInteger("admin_id");
+            $table->foreign("admin_id")->references("admin_id")
+                ->on("admins")->onDelete("cascade")->onUpdate("cascade");
+            $table->date('dateLog');
+            $table->string('descriptionLog');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
