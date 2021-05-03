f338abb8624186c5653751977810f97107479072
chraibibadr <badr.chraibibadr@gmail.com>
2021-05-03 22:51:43 +0000

    User Controller Update

--- BACKEND/app/Http/Controllers/UserController.php
+++ BACKEND/app/Http/Controllers/UserController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 
 class UserController extends Controller
 {
     //
     public function index(){
-    $vue = User::all();
-	return $vue;
+        $users = DB::select('select * from users');
+
+        return $users;
 
     }
 }
