f20f6f8e2d9db1a26ddc371deb1e8f4483fdb349
Aitalla Mohamed <maygarayan17@gmail.com>
2021-05-07 01:04:21 +0100

    BACKEND-UPDATE

--- BACKEND/app/Http/Controllers/UserController.php
+++ BACKEND/app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Validator;
 
 
 class UserController extends Controller
@@ -17,11 +19,35 @@ class UserController extends Controller
     }
     public static function register(Request $request){
 
-        if($request->isPatient == "true")
-            DB::insert('insert into users (email, password, type) values (?, ?, ?)', [$request->email, $request->password, 'patient']);
-        elseif($request->isPatient == "false")
-            DB::insert('insert into users (email, password, type) values (?, ?, ?)', [$request->email, $request->password, 'medecin']);
-            
-        return response()->json($request);
+      
+
+     
+           
+        $count = User::where('email','=',$request->email)->count();
+
+        if($count == 1)
+        {
+            return response()->json([
+                'success' => '',
+                'error' => 'Duplicate '
+            ]);
+        }
+        if($count == 0){
+        
+            if($request->isPatient == "true")
+                 DB::insert('insert into users (email, password, type) values (?, ?, ?)', [$request->email, $request->password, 'patient']);
+             elseif($request->isPatient == "false")
+                 DB::insert('insert into users (email, password, type) values (?, ?, ?)', [$request->email, $request->password, 'medecin']);
+        
+                 return response()->json([
+                    'success' => 'done ',
+                    'error' => ' '
+                ]);
+        
+        }
+ 
+   
+
+        
     }
 }
