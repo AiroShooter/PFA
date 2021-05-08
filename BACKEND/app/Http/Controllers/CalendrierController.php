56b1576d6f9efbd90ab7b4af629553f16f86d475
ayoub <youbchaf@gmail.com>
2021-05-08 00:07:03 +0000

    migration update1

---
+++ BACKEND/app/Http/Controllers/CalendrierController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\calendrier;
+use Illuminate\Http\Request;
+
+class CalendrierController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\calendrier  $calendrier
+     * @return \Illuminate\Http\Response
+     */
+    public function show(calendrier $calendrier)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\calendrier  $calendrier
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(calendrier $calendrier)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\calendrier  $calendrier
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, calendrier $calendrier)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\calendrier  $calendrier
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(calendrier $calendrier)
+    {
+        //
+    }
+}
