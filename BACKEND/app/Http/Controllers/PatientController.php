eabc79178f68405b07ad232f87b169e06bc175ca
ayoub <youbchaf@gmail.com>
2021-05-08 11:34:16 +0000

    pa

--- BACKEND/app/Http/Controllers/PatientController.php
+++ BACKEND/app/Http/Controllers/PatientController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Patient;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 class PatientController extends Controller
 {
@@ -22,9 +24,27 @@ class PatientController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
+        DB::insert('insert into patients (`user_id`, `spec_id`, `titre`, `nom`, `prenom`, `tarif`, `siteWeb`, `adresseCabinet`, `ville`, `teleCabinet`, `telePerso`, `duree`) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [$request->spec_id, $request->titre, $request->nom, $request->prenom, $request->tarif, $request->siteWeb, $request->adresseCabinet, $request->ville, $request->teleCabinet, $request->telePerso, $request->duree]);
+
+        $users = DB::select('select * from medecins where user_id = ?',[$request->user_id]);
+        if($users)
+        {
+           return response()->json([
+               'hasError' => false,
+               'success' => 'Done ',
+               'error' => '',
+               'user' =>$users[0]]);
+        }
+        else{
+            return response()->json([
+                'hasError' => true,
+                'success' => '',
+                'error' => 'Error']);
+        }
+        
+
     }
 
     /**
