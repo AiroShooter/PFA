affcc98e9ad365d3ebecc53263674ad3d4f2c1ee
Aitalla Mohamed <maygarayan17@gmail.com>
2021-05-15 23:43:29 +0100

    SCADULE-TIMING

--- FRONTEND/src/app/doctor/scheduletiming/scheduletiming.module.ts
+++ FRONTEND/src/app/doctor/scheduletiming/scheduletiming.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule,NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ScheduletimingRoutingModule } from './scheduletiming-routing.module';
 import { ScheduletimingComponent } from './scheduletiming.component';
 import { NgSelect2Module } from 'ng-select2';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
+  schemas:[NO_ERRORS_SCHEMA],
   declarations: [ScheduletimingComponent],
-  imports: [NgSelect2Module, CommonModule, ScheduletimingRoutingModule],
+  imports: [NgSelect2Module, CommonModule, ScheduletimingRoutingModule, FormsModule],
 })
 export class ScheduletimingModule {}
