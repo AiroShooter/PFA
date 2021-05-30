00b9447241f79e490558a3cc987d7e29b8b54ff0
chraibibadr <badr.chraibibadr@gmail.com>
2021-05-30 20:10:19 +0100

    laaaaaaaast up

--- FRONTEND/src/app/forgot-password/forgot-password.component.ts
+++ FRONTEND/src/app/forgot-password/forgot-password.component.ts
@@ -16,17 +16,17 @@ export class ForgotPasswordComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  SERVER_URL: string = 'http://127.0.0.1:8000/api/';
-    myForm = this.fb.group({
-      email:['']
-    });
+  // SERVER_URL: string = 'http://127.0.0.1:8000/api/';
+  //   myForm = this.fb.group({
+  //     email:['']
+  //   });
 
-  ForgetPass(){
-    let form = new FormData();
-    form.append("email",this.myForm.value.email);
-    form.append("password","1234567891");
-    this.http.post(this.SERVER_URL + 'ForgetPass', form).subscribe(result =>{
-      console.log(result);
-    });
-  }
+  // ForgetPass(){
+  //   let form = new FormData();
+  //   form.append("email",this.myForm.value.email);
+  //   form.append("password","1234567891");
+  //   this.http.post(this.SERVER_URL + 'ForgetPass', form).subscribe(result =>{
+  //     console.log(result);
+  //   });
+  // }
 }
